7f314a500e8a730cd74e03e7e9e58158ee54907a
Leonel De Amorim <deamorimleonel@gmail.com>
2026-08-19 22:26:12 -0400

    Rename to proper .ts
    Added node server connection.

--- momentum-mod-showpos/show-pos.ts
+++ momentum-mod-showpos/show-pos.ts
@@ -2,6 +2,55 @@ import { PanelHandler } from 'util/module-helpers';
 import { CustomizerPropertyType, registerHUDCustomizerComponent } from 'common/hud-customizer';
 import { getTextShadowFast } from 'common/hud-customizer';
 
+
+//Server 
+const ServerPost = () => {
+    const angles = MomentumPlayerAPI.GetAngles();
+    const velocity = MomentumPlayerAPI.GetVelocity();
+    const energy = MomentumPlayerAPI.GetEnergy();
+    const strafeSync0 = MomentumPlayerAPI.GetStrafeSync(0);
+    const strafeSync1 = MomentumPlayerAPI.GetStrafeSync(1);
+    const moveType = MomentumMovementAPI.GetMoveType();
+    const moveHud = MomentumMovementAPI.GetMoveHudData();
+    const lastTick = MomentumMovementAPI.GetLastTickStats();
+    const currentTime = MomentumMovementAPI.GetCurrentTime();
+    const ducking = MomentumPlayerAPI.IsDucking();
+    const sprinting = MomentumPlayerAPI.IsSprinting();
+    const walking = MomentumPlayerAPI.IsWalking();
+    const buttons = MomentumInputAPI.GetButtons();
+
+    const data = {
+        time: currentTime,
+        angles: JSON.stringify(angles),
+        velocity: JSON.stringify(velocity),
+        energy: energy,
+        strafeSync0: strafeSync0,
+        strafeSync1: strafeSync1,
+        moveType: moveType,
+        moveHud: JSON.stringify(moveHud),
+        lastTick: JSON.stringify(lastTick),
+        ducking: ducking,
+        sprinting: sprinting,
+        walking: walking,
+
+        buttons: JSON.stringify(buttons)
+    };
+
+    $.AsyncWebRequest('http://127.0.0.1:8080/test', {
+        type: 'POST',
+        data: data,
+    }as any);
+};
+
+
+const requestLoop = () => {
+    ServerPost();
+
+    $.Schedule(0, requestLoop);
+};
+
+
+
 @PanelHandler()
 class HudShowPosHandler {
 	constructor() {
