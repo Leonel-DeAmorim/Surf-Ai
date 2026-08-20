904f09db0ca1e76ce50d0a7973f1635f889c1756
Leonel De Amorim <deamorimleonel@gmail.com>
2026-08-19 22:32:20 -0400

    Import Added

--- momentum-mod-showpos/show-pos.ts
+++ momentum-mod-showpos/show-pos.ts
@@ -1,5 +1,6 @@
 import { PanelHandler } from 'util/module-helpers';
 import { CustomizerPropertyType, registerHUDCustomizerComponent } from 'common/hud-customizer';
+import { getTextShadowFast } from 'common/hud-customizer';
 
 
 //Server 
