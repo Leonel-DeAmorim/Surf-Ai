817a749fa4c04934981d470636b969b91a8c5dff
Leonel De Amorim <deamorimleonel@gmail.com>
2026-09-14 22:13:10 -0400

    Implement BSP file reading in bsp_parser.py
    Add functionality to read and print data from a BSP file.

--- BSP-LIDAR/bsp_parser.py
+++ BSP-LIDAR/bsp_parser.py
@@ -1,1 +1,8 @@
 //contents for bsp parser
+file_path = r"C:\Program Files (x86)\Steam\steamapps\common\Momentum Mod Playtest\momentum\maps\surf_mom_training.bsp"
+
+with open(file_path, "rb") as f:
+    data = f.read()
+
+print(len(data))
+print(data[:4])
