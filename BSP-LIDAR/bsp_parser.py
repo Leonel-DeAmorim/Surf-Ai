a23c528847e3f50ba8864cc59896d94898eeb32e
Leonel De Amorim <deamorimleonel@gmail.com>
2026-09-16 18:39:41 -0400

    Refactor BSP parser to include side and find_leaf functions
    Changes leaf_index to -1 from index to get correct leaf. Tested leaf with test position and found given out position that the leaf was 256 as it fell within its bounds. Previous abs implementation did not.

--- BSP-LIDAR/bsp_parser.py
+++ BSP-LIDAR/bsp_parser.py
@@ -2,28 +2,32 @@ import bsp_tool
 
 bsp = bsp_tool.load_bsp(r"c:\Program Files (x86)\Steam\steamapps\common\Momentum Mod Playtest\momentum\maps\surf_mom_training.bsp")
 
-plane = bsp.PLANES[bsp.NODES[1].plane]
+position = (5000, 1000, 0)
 
-print(plane)
+def side(position, plane):
+  side =  (position[0]*plane.normal[0]+ position[1]*plane.normal[1]+position[2]*plane.normal[2])-plane.distance
+  return side
 
-point1 = (5000, 1000, 0)
-point2 = (5000, -1000, 0)
+def find_leaf(position, node_index):
 
-for point in [point1, point2]:
-    result = (
-        point[0] * plane.normal[0]
-        + point[1] * plane.normal[1]
-        + point[2] * plane.normal[2]
-        - plane.distance
-    )
+ if(node_index <0):
+    leaf_index=-node_index-1
+    return leaf_index
+  
+ node = bsp.NODES[node_index]
+ plane = bsp.PLANES[node.plane]
+ 
+ if side(position,plane)>=0:
+     next_node_index= node.children[0]
+ else:
+     next_node_index= node.children[1]
 
-node = bsp.NODES[2]
+ return find_leaf(position, next_node_index)
 
-print("Plane:", bsp.PLANES[node.plane])
-print("Children:", node.children)
+node = bsp.NODES[0]
+plane = bsp.PLANES[node.plane]
 
-for child in node.children:
-    child_node = bsp.NODES[child]
-    print("Child:", child)
-    print("Min:", child_node.bounds.mins)
-    print("Max:", child_node.bounds.maxs)
+print("Distance:",side(position, plane))
+print("In node:",find_leaf(position, 0))
+print(bsp.LEAVES[256])
+print(bsp.LEAVES[257])
