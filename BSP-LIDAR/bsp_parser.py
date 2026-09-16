1193c29f1252154487d0efe2871d97190489e50b
Leonel De Amorim <deamorimleonel@gmail.com>
2026-09-16 16:53:51 -0400

    Enhance bsp_parser with plane and node information
    Added calculations for plane intersection and node details.

--- BSP-LIDAR/bsp_parser.py
+++ BSP-LIDAR/bsp_parser.py
@@ -2,9 +2,28 @@ import bsp_tool
 
 bsp = bsp_tool.load_bsp(r"c:\Program Files (x86)\Steam\steamapps\common\Momentum Mod Playtest\momentum\maps\surf_mom_training.bsp")
 
-print("BSP version:", bsp.version)
-print("Number of planes:", len(bsp.PLANES))
-print("Number of nodes:", len(bsp.NODES))
-print("Number of leaves:", len(bsp.LEAVES))
-print("Number of vertices:", len(bsp.VERTICES))
-print("Number of faces:", len(bsp.FACES))
+plane = bsp.PLANES[bsp.NODES[1].plane]
+
+print(plane)
+
+point1 = (5000, 1000, 0)
+point2 = (5000, -1000, 0)
+
+for point in [point1, point2]:
+    result = (
+        point[0] * plane.normal[0]
+        + point[1] * plane.normal[1]
+        + point[2] * plane.normal[2]
+        - plane.distance
+    )
+
+node = bsp.NODES[2]
+
+print("Plane:", bsp.PLANES[node.plane])
+print("Children:", node.children)
+
+for child in node.children:
+    child_node = bsp.NODES[child]
+    print("Child:", child)
+    print("Min:", child_node.bounds.mins)
+    print("Max:", child_node.bounds.maxs)
