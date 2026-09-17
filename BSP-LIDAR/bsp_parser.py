434efd5b36e97e83108d9bd6cf31f7ac3a472fe2
Leonel De Amorim <deamorimleonel@gmail.com>
2026-09-17 10:45:31 -0400

    Add comments for clarity in bsp_parser.py
    Added comments to clarify the purpose of the code and the logic behind the functions.

--- BSP-LIDAR/bsp_parser.py
+++ BSP-LIDAR/bsp_parser.py
@@ -1,27 +1,28 @@
 import bsp_tool
-
+#Load the .bsp map using bsp_tool
 bsp = bsp_tool.load_bsp(r"c:\Program Files (x86)\Steam\steamapps\common\Momentum Mod Playtest\momentum\maps\surf_utopia.bsp")
-
+#Define a 3D coordinate point (X,Y,Z) to test within map space
 position = (13796.82, 16.80, 12864.03)
-
+#Given position and plane we do the dot product and subtract it from the distance to find on which side of the plane our position is
 def side(position, plane):
   side =  (position[0]*plane.normal[0]+ position[1]*plane.normal[1]+position[2]*plane.normal[2])-plane.distance
   return side
-
+#Define recursive function to go through the bsp and find the leaf our position is in
 def find_leaf(position, node_index):
-
+#If we get a negative value then that tells use we reached the leaf
+#Leaf nodes are encoded as -(node +1) so when we get a leaf -256 its node index is 255
  if(node_index <0):
     leaf_index=-node_index-1
     return leaf_index
   
  node = bsp.NODES[node_index]
  plane = bsp.PLANES[node.plane]
- 
+ #If out side value is positive then we move left down the BSP tree else we move right
  if side(position,plane)>=0:
      next_node_index= node.children[0]
  else:
      next_node_index= node.children[1]
-
+#return our lead node index
  return find_leaf(position, next_node_index)
 
 node = bsp.NODES[0]
