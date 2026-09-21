f8c9139a21960999a7cd48957cd6f6f1b6bdbf43
Leonel De Amorim <deamorimleonel@gmail.com>
2026-09-21 11:57:52 -0400

    Extracted Map Geometry
    Added logic to extract and store geometry from BSP faces. Now we can a face and get its geometry.

--- BSP-LIDAR/bsp_parser.py
+++ BSP-LIDAR/bsp_parser.py
@@ -2,7 +2,7 @@ import bsp_tool
 #Load the .bsp map using bsp_tool
 bsp = bsp_tool.load_bsp(r"c:\Program Files (x86)\Steam\steamapps\common\Momentum Mod Playtest\momentum\maps\surf_utopia.bsp")
 #Define a 3D coordinate point (X,Y,Z) to test within map space
-position = (13796.82, 16.80, 12864.03)
+position = (-12720.69, -5.97, 11479.33)
 #Given position and plane we do the dot product and subtract it from the distance to find on which side of the plane our position is
 def side(position, plane):
   side =  (position[0]*plane.normal[0]+ position[1]*plane.normal[1]+position[2]*plane.normal[2])-plane.distance
@@ -25,10 +25,29 @@ def find_leaf(position, node_index):
 #return our lead node index
  return find_leaf(position, next_node_index)
 
-node = bsp.NODES[0]
-plane = bsp.PLANES[node.plane]
+map_geometry = []
 
-print("Distance:",side(position, plane))
-print("In node:",find_leaf(position, 0))
-print(bsp.LEAVES[557])
+for face_index, face in enumerate(bsp.FACES):
+    surfedges = bsp.SURFEDGES[
+       face.first_edge:
+       face.first_edge + face.num_edges
+    ]
+    vertices = []
 
+    for surfedge in surfedges:
+        edge_index = abs(surfedge)
+        edge = bsp.EDGES[edge_index]
+        if surfedge >= 0:
+           vertex_index = edge[0]
+        else: 
+           vertex_index = edge[1]
+        vertex = bsp.VERTICES[vertex_index]
+        vertices.append(vertex)
+
+    if len(vertices) >= 3:
+       map_geometry.append((face_index, vertices))
+
+print("BSP FACES:", len(bsp.FACES))
+print("Total stored geometry:", len(map_geometry))
+print("First entry:", map_geometry[0])
+print("Last entry:", map_geometry[-1])
